979d4698cef0cfdf41b6a78e69591dfdda67518d
sowmyabilla <sowmyabillakanti@gmail.com>
2022-01-15 10:33:40 +0530

    first commit

---
+++ src/Review.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'
+import people from './Data';
+import { FaChevronLeft,FaChevronRight,FaQuoteRight} from 'react-icons/fa';
+const Review = () => {
+    const[index,setIndex] = useState(0);
+const {name,category,image,text} = people[index];
+
+const checkNumber = (number) => {              //number= 0,1,2,3,4,5
+    if (number > people.length-1){             //people.length=6
+        return 0;
+    }
+    if (number<0){
+        return people.length -1;
+    }
+    return number;
+}
+
+const nextPerson = () => {
+    setIndex((index)=>{
+        let newIndex = index +1;
+        return checkNumber(newIndex);
+    });
+};
+const prevPerson = () => {
+    setIndex((index)=>{
+        let newIndex = index -1;
+        return checkNumber(newIndex);
+    });
+};
+const randomPerson = ( )=> {
+    let randomNumber=Math.floor(Math.random() * people.length);
+if(randomNumber === index){
+    randomNumber=index+1;
+}
+setIndex(checkNumber(randomNumber));
+}
+    return (
+        <article className='review'>
+            <div className="img-container">
+                <img src={image} alt={name} className='person-img'></img>
+                <span className='quote-icon'>
+                    <FaQuoteRight />
+                </span>
+            </div>
+            <h4 className='author'>{name}</h4>
+            <p className='job'>{category}</p>
+            <p className='info'>{text}</p>
+            <div className="button-container">
+                <button className='prev-btn' onClick={prevPerson}>
+                    <FaChevronLeft />
+                </button>
+                <button className='next-btn'onClick={nextPerson}>
+                    <FaChevronRight />
+                </button>
+            </div>
+            <button className='random-btn' onClick={randomPerson}>
+                   Suprise Me
+                </button>
+                {/* <div>{people.length}</div> */}
+        </article>
+    );
+}
+
+export default Review;
